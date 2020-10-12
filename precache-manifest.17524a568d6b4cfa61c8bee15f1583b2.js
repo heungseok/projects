cd5a9ad47a09a264ecaacc286cf8aadcf2f2b4d4
heungseok <heungseok2@gmail.com>
2020-10-12 20:31:29 +0900

    Updates

---
+++ precache-manifest.17524a568d6b4cfa61c8bee15f1583b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18893cfc82794f990eb0bed4f418343a",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "a79718fc7afa98ef6b4a",
+    "url": "/projects/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ea370e1a45fd1f547f9",
+    "url": "/projects/static/js/2.8394f818.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projects/static/js/2.8394f818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79718fc7afa98ef6b4a",
+    "url": "/projects/static/js/main.65424edf.chunk.js"
+  },
+  {
+    "revision": "f639c188cb940516bcf1",
+    "url": "/projects/static/js/runtime-main.da0864a9.js"
+  }
+]);
