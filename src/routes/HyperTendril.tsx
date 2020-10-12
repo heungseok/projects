aa28aefedaccf4a1b45aedbfe0af5328ec318894
heungseok <heungseok2@gmail.com>
2020-10-12 20:34:28 +0900

    init projects page

---
+++ src/routes/HyperTendril.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HyperTendril() {
+
+  return (
+    <div>
+      HyperTendril page
+    </div>
+  )
+}
---
+++ src/routes/Projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Projects() {
+
+  return (
+    <div>
+      This page is for project lists
+    </div>
+  )
+}
